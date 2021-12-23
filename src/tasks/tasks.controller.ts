02effc5de78949ea22a591ee91e7ead3f33b139d
Jossic <jossic.lapierre@gmail.com>
2021-12-23 21:50:19 +0100

    refactoring updateTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -43,12 +43,12 @@ export class TasksController {
     return this.tasksService.deleteTask(id);
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status') updateTaskStatusDTO: updateTaskStatusDTO,
-  // ): Task {
-  //   const { status } = updateTaskStatusDTO;
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') updateTaskStatusDTO: updateTaskStatusDTO,
+  ): Promise<Task> {
+    const { status } = updateTaskStatusDTO;
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 }
